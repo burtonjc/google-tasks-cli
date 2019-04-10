d6dae9077f056f6802a212c920c8f9c27af67271
Jacob Burton <burtonjc35@gmail.com>
2019-04-09 17:50:59 -0800

    add "list" flag to the "list" command

--- src/commands/list/index.ts
+++ src/commands/list/index.ts
@@ -5,6 +5,7 @@ import * as moment from 'moment';
 
 import { CommandExecutor } from '../../helpers/command-helper';
 import { getTasksV1Client } from '../../helpers/google-helper';
+import { tasks_v1 } from 'googleapis';
 
 const executeCommand: CommandExecutor = async () => {
   const cli = meow(`
@@ -41,38 +42,48 @@ const executeCommand: CommandExecutor = async () => {
   const TasksV1 = getTasksV1Client();
 
   const { data: lists } = await TasksV1.tasklists.list();
-
-  for (const list of lists.items) {
-    const table = new Table({
-      colWidths: [3, 5, 50, 12],
-      chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
-      , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
-      , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
-      , 'right': '' , 'right-mid': '' , 'middle': ' ' },
-      style: { 'padding-left': 0, 'padding-right': 0 }
-    });
-
-    const { data: tasks } = await TasksV1.tasks.list({
-      tasklist: list.id,
-      showCompleted: cli.flags.showHidden,
-      showHidden: cli.flags.showHidden,
-    });
-
-    if (!tasks.items) { continue; }
-
-    for (const task of tasks.items) {
-      table.push([
-        task.status === 'completed' ? `[${chalk.green('x')}]` : '[ ]',
-        chalk.grey(task.id.substr(-5)),
-        task.title,
-        moment.utc(task.updated).fromNow()
-      ])
+  if (cli.flags.list) {
+    const list = lists.items.find(l =>
+      l.title.toLowerCase() === cli.flags.list.toLowerCase()
+    );
+    printTaskListItems(cli, TasksV1, list);
+  } else {
+    for (const list of lists.items) {
+      printTaskListItems(cli, TasksV1, list);
     }
-
-    console.log();
-    console.log(chalk.blue.underline(list.title));
-    console.log(table.toString());
   }
+}
+
+const printTaskListItems = async (cli: meow.Result, client: tasks_v1.Tasks, list: tasks_v1.Schema$TaskList) => {
+  const table = new Table({
+    colWidths: [3, 5, 50, 12],
+    chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
+    , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
+    , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
+    , 'right': '' , 'right-mid': '' , 'middle': ' ' },
+    style: { 'padding-left': 0, 'padding-right': 0 }
+  });
+
+  const { data: tasks } = await client.tasks.list({
+    tasklist: list.id,
+    showCompleted: cli.flags.showHidden,
+    showHidden: cli.flags.showHidden,
+  });
+
+  if (!tasks.items) { return; }
+
+  for (const task of tasks.items) {
+    table.push([
+      task.status === 'completed' ? `[${chalk.green('x')}]` : '[ ]',
+      chalk.grey(task.id.substr(-5)),
+      task.title,
+      moment.utc(task.updated).fromNow()
+    ])
+  }
+
+  console.log();
+  console.log(chalk.blue.underline(list.title));
+  console.log(table.toString());
 }
 
 export default executeCommand;
